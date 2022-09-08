ad4f1906244b3adbdeb31a78e02106727e3e6749
buhlebaloyi <buhlebaloyi1@gmail.com>
2022-09-08 22:17:13 +0200

    function work except for one test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
 const removeFromArray = function() {
-
+    initialArray = arguments[0];
+    let someArray = [];
+    for (array in initialArray) {
+        noOfExtraArguments = arguments.length - 1;
+        while (noOfExtraArguments >= 0){
+            if (initialArray[array] === arguments[noOfExtraArguments]){
+                initialArray.splice(array, 1);
+                noOfExtraArguments--;
+            } else {
+                someArray.push(arguments[noOfExtraArguments]);
+                noOfExtraArguments--;
+            }
+        }
+    }
+    return initialArray;
 };
 
 // Do not edit below this line
