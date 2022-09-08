9d7341607c8c02de6ce15243e39d03ffaca3e5b1
buhlebaloyi <buhlebaloyi1@gmail.com>
2022-09-08 09:36:20 +0200

    repeatString function complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,times) {
+    let i = 0;
+    let stringOut = "";
+    while (i < times) {
+        stringOut += string;
+        i++;
+    }
+    return stringOut;
 };
 
 // Do not edit below this line
