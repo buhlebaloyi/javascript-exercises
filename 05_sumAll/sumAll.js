2c7c7757b00691b19eafeb713af57173b0352c73
buhlebaloyi <buhlebaloyi1@gmail.com>
2022-09-15 23:57:48 +0200

    Pass three first tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
+    console.log(num1, num2);
+
+    if (num1 < num2){
+        for (let i = num1; i <= num2; i++){
+            sum += i;
+            console.log(sum);
+        }
+    } else {
+        for (let i = num2; i <= num1; i++){
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
