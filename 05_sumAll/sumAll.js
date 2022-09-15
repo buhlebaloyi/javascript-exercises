e31ab7118ae5bbd954fa1117140aef560667e536
buhlebaloyi <buhlebaloyi1@gmail.com>
2022-09-16 00:19:55 +0200

    Removing console logs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,5 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
-    console.log(num1, num2);
 
     if (num1 < 0 || num2 < 0){
         return "ERROR";
@@ -12,12 +11,10 @@ const sumAll = function(num1, num2) {
         if (num1 < num2){
             for (let i = num1; i <= num2; i++){
                 sum += i;
-                //console.log(sum);
             }
         } else {
             for (let i = num2; i <= num1; i++){
                 sum += i;
-                console.log(sum);
             }
         }
     
