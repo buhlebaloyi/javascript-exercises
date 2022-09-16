481663f2e51ec2f654bc95b3823aaed82316f547
buhlebaloyi <buhlebaloyi1@gmail.com>
2022-09-16 08:30:42 +0200

    All tests work

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let temperatureF = (temperature - 32) * (5/9);
+  temperatureF = Math.round(temperatureF * 10) / 10;
+  return temperatureF;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let temperatureC = temperature * (9/5) + 32;
+  temperatureC = Math.round(temperatureC * 10) / 10;
+  return temperatureC;s
 };
 
 // Do not edit below this line
